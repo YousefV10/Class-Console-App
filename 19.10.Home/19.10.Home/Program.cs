8fd8154852ed8ca6540195fc61016db57cf850fb
YousefV10 <yusifrv-bpa202@code.edu.az>
2025-10-19 22:21:14 +0400

    I did'n Save the Repos

--- 19.10.Home/19.10.Home/Program.cs
+++ 19.10.Home/19.10.Home/Program.cs
@@ -1,10 +1,147 @@
-﻿namespace _19._10.Home
-{
-    internal class Program
+﻿
+
+namespace _20._10_HomeTask
     {
-        static void Main(string[] args)
+        internal class Program
         {
-            Console.WriteLine("Hello, World!");
+            static void Main(string[] args)
+            {
+
+                bool check = true;
+                Student student = new Student("Yusif ", "Veliyev", 20, 67);
+                Student student1 = new Student("Ulker ", "Veliyeva ", 18, 96);
+                Student student2 = new Student("Tahir ", "Eliyev", 19, 75);
+                Student[] students = { student, student1, student2 };
+
+                foreach (var person in students)
+                {
+
+                    Console.WriteLine($"{person.Name} , {person.Surname},  {person.Age}, {person.Grade}");
+                }
+
+
+
+
+                while (check)
+                {
+                    Console.WriteLine(1 + ".Create New Student");
+                    Console.WriteLine(2 + ".Delete Student ");
+                    Console.WriteLine(3 + ".Print All Students");
+                    Console.WriteLine(4 + ".Print Fav Students");
+                    Console.WriteLine(0 + ".Exit");
+
+                    int number = int.Parse(Console.ReadLine());
+                    switch (number)
+                    {
+                        case 1:
+
+                            Console.WriteLine("Enter the name ");
+                            string name3 = Console.ReadLine();
+
+                            Console.WriteLine("Enter the Surname ");
+                            string surname3 = Console.ReadLine();
+
+                            Console.WriteLine("Enter the Age ");
+                            int age3 = int.Parse(Console.ReadLine());
+
+                            Console.WriteLine("Enter the Gradde");
+                            int grade3 = int.Parse(Console.ReadLine());
+
+                            Student student3 = new Student(name3, surname3, age3, grade3);
+
+                            Array.Resize(ref students, students.Length + 1);
+                            students[students.Length - 1] = student3;
+                            break;
+
+
+
+
+                        case 2:
+                            Console.WriteLine("Enter the student ");
+                            int del = int.Parse(Console.ReadLine());
+
+                            if (del > students.Length)
+                            {
+                                Console.WriteLine("Student not found ");
+                                break;
+                            }
+                            for (int i = del; i < students.Length; i++)
+                            {
+                                students[i - 1] = students[i];
+                            }
+                            Array.Resize(ref students, students.Length - 1);
+
+                            foreach (var person in students)
+                            {
+
+                                Console.WriteLine($"{person.Name}, {person.Surname}, {person.Age}, {person.Grade}");
+                            }
+
+                            break;
+
+
+
+                        case 3:
+                            foreach (var person in students)
+                            {
+
+                                Console.WriteLine($"{person.Name}, {person.Surname}, {person.Age}, {person.Grade}");
+                            }
+
+                            break;
+
+
+
+                        case 4:
+
+                            int index = 0;
+                            for (int i = 0; i <= students.Length; i++)
+                            {
+                                if (students[i].Grade > 90)
+                                {
+
+                                    index = i;
+                                    Console.WriteLine($"{students[index].Name}, {students[index].Surname}, {students[index].Age}, {students[index].Grade}");
+                                }
+
+
+
+
+
+
+                            }
+
+                            break;
+
+
+
+                        case 0:
+
+                            check = false;
+
+                            break;
+                    }
+
+
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
         }
+    }
+
+}
     }
 }
--- 19.10.Home/19.10.Home/Student.cs
+++ 19.10.Home/19.10.Home/Student.cs
@@ -1,12 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace _19._10.Home
+namespace _20._10_HomeTask
 {
     internal class Student
     {
+        public string Name;
+        public string Surname;
+        public int Age;
+        public int Grade;
+
+
+        public Student(string name, string surname, int age, int grade)
+        {
+            Name = name;
+
+            if (surname.Length < 3)
+            {
+                Surname = surname;
+            }
+            else
+            {
+                Surname = "XXXXXXX";
+            }
+
+            if (age < 0)
+            {
+                Age = 0;
+
+            }
+            else
+            {
+                Age = age;
+            }
+
+            if (grade >= 0 && grade <= 100)
+            {
+                Grade = grade;
+
+            }
+            else if (grade < 0)
+            {
+                Grade = 0;
+            }
+            else
+            {
+                Grade = 100;
+            }
+
+
+        }
     }
 }
