128c1066aee617162c26ff0c18babc3646c3c77b
YousefV10 <yusifrv-bpa202@code.edu.az>
2025-10-19 22:18:01 +0400

    Home Task

---
+++ 19.10.Home/19.10.Home/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _19._10.Home
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
