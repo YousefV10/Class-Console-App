ee40e46b6625f79cf46be2679bf32b36802b702b
YousefV10 <yusifrv-bpa202@code.edu.az>
2025-11-07 13:49:22 +0400

    TAsk2

---
+++ IDK2/IDK2/CarService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDK2
+{
+    internal class CarService
+    {
+        public delegate void ServiceDelegate();
+
+        public ServiceDelegate ServiceChain;
+
+        public void PerformService()
+        {
+            ServiceChain?.Invoke();
+
+            if (ServiceChain == null)
+            {
+                Console.WriteLine(" Please choose operation");
+            }
+
+            Console.WriteLine("Car check ended ");
+        }
+
+        public void CheckOil()
+        {
+            Console.WriteLine("Oil checked");
+        }
+
+        public void CheckBrakes()
+        {
+            Console.WriteLine(" Brake Checked");
+        }
+
+        public void CheckEngine()
+        {
+            Console.WriteLine("Engine Checked ");
+        }
+
+        public void CheckLights()
+        {
+            Console.WriteLine("  Lights Checked ");
+        }
+    }
+}
+
---
+++ IDK2/IDK2/Program.cs
@@ -0,0 +1,37 @@
+﻿using IDK2;
+
+ static bool AskForCheck(string checkName)
+{
+    Console.Write($"{checkName} is true ?  ");
+    return Console.ReadLine()?.Trim().ToLower() == "y";
+}
+
+
+    var carService = new CarService();
+
+    Console.WriteLine("Please Choose one ");
+
+    if (AskForCheck("Oil "))
+    {
+        carService.ServiceChain += carService.CheckOil;
+    }
+
+    if (AskForCheck("Brake "))
+    {
+        carService.ServiceChain += carService.CheckBrakes;
+    }
+
+    if (AskForCheck("Engine "))
+    {
+        carService.ServiceChain += carService.CheckEngine;
+    }
+
+    if (AskForCheck("Lights"))
+    {
+        carService.ServiceChain += carService.CheckLights;
+    }
+
+    carService.PerformService();
+
+    Console.ReadLine();
+
